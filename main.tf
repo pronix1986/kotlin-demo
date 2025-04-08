928562b90e84d64d084d3af7a0f08eca8c181566
Siarhei_Prakharenka <siarhei.prakhareanka@acertusdelivers.com>
2025-04-08 17:38:43 +0300

    local provider base setup

--- main.tf
+++ main.tf
@@ -1,3 +1,20 @@
 terraform {
 
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "2.5.1"
+    }
+  }
 }
+
+provider "local" {}
+
+data "local_file" "readme" {
+  filename = "README.md"
+}
+
+output "file_path" {
+  value = data.local_file.readme.filename
+}
+
